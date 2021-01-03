c2d8d7f273cbfe1c9f29d990842b07af1476fadc
Tineshnehete <74779225+Tineshnehete@users.noreply.github.com>
2021-01-03 15:18:54 +0530

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -12,10 +12,8 @@ function highlight_html(){
        data=data.replace(/(&equalsa)/g,'<span class="eql">=</span>').replace(/&aquota(.*?)&aquota/gs,'<span class="str">"$1"</span>').replace(/&aaposa(.*?)&aaposa/gs,"<span class='str'>'$1'</span>").replace(/([\r\n])/g,'$1<line></line>').replace(/&equalsa/,'=');   
        
        
-       /*
-          data=data.replace(/&aquot;(.*?)&aquot;/g,'<span class="str">"$1"</span>')
-          data=data.replace(/&aapos;(.*?)&aapos;/g,'<span class="str">\'$1\'</span>')
-          */
+  
+        
       data=data.replace(/([A-Za-z0-9-_.]+):(.*?)([;])/g,'<span class="css1">$1</span>:<span class="css">$2</span>$3').replace(/(http|https)&colona/g,'$1:')
       
     ba[i].innerHTML=' <button onclick="CopyToClipboard(\'code\','+i+');return false;" class="btn copytoboard">Copy  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-data" viewBox="0 0 16 16">  <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0V7zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V9z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/> <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg></button><br>'
